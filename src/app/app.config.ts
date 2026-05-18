91b0bc0817d22448cd4e79d61a5a0b0dedd9fc4a
DESKTOP-NHGP7JL\Rafal2 <rafalkaczmarek09@gmail.com>
2026-05-18 14:52:18 +0200

    fix: update dependency version and enhance HTTP client configuration
    - Changed the version of @axe-core/playwright to a more specific version using the tilde (~) to ensure compatibility.
    - Added HTTP client configuration to the application setup by providing the HttpClient with fetch support, improving data handling capabilities.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,4 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -6,6 +7,7 @@ import { routes } from '@admin-panel-web/app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withFetch()),
     provideRouter(routes),
   ]
 };
